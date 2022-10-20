21b7e1747ccaba27e3c6cf0109ccd3091ec51c52
Michael Sepsey <msepsey@umich.edu>
2022-10-20 16:47:45 -0400

    Master changed values of a, b, and c in quraticsolver.java

--- quraticsolver.java
+++ quraticsolver.java
@@ -3,9 +3,9 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 4;
-		int b = 12;
-		int c = 7;
+		int a = 2;
+		int b = 9;
+		int c = 6;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
